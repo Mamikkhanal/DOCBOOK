d0ed6426cd6df6d4583afe0d8484df87e3ec863c
mamikkhanal <mamik.khanal8@gmail.com>
2024-11-19 14:20:48 +0545

    [Schema]:added schemas

---
+++ app/Http/Controllers/AppointmentSlotController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AppointmentSlot;
+use Illuminate\Http\Request;
+
+class AppointmentSlotController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AppointmentSlot $appointmentSlot)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AppointmentSlot $appointmentSlot)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AppointmentSlot $appointmentSlot)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AppointmentSlot $appointmentSlot)
+    {
+        //
+    }
+}
